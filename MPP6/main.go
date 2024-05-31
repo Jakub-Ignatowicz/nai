76bbf870f3bd553ce409c3c0f33f61a3d62fa915
Jakub-Ignatowicz <kuba_ign@proton.me>
2024-05-31 17:00:04 +0200

    feat(mpp6): implement letter counter in main

--- MPP6/main.go
+++ MPP6/main.go
@@ -1,5 +1,33 @@
 package main
 
-func main() {
+import (
+	"container/heap"
+	"mpp6/utils"
+)
 
+func countLetters(sentence string) map[rune]int {
+	letterCount := make(map[rune]int)
+
+	// Use range to iterate over each rune in the string
+	for _, char := range sentence {
+		letterCount[char]++
+	}
+
+	return letterCount
+}
+
+func main() {
+	letterCounts := countLetters("ala ma kota")
+
+	pq := make(utils.PriorityQueue, len(letterCounts))
+
+	i := 0
+	for key, value := range letterCounts {
+		pq[i] = utils.Item{
+			Name:   key,
+			Weight: value,
+		}
+		i++
+	}
+	heap.Init(&pq)
 }
