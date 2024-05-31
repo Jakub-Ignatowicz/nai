a6a3b7b7cf201aea5762c32f117a2067cc3e5918
Jakub-Ignatowicz <kuba_ign@proton.me>
2024-05-31 23:09:45 +0200

    feat(mpp6): implement hoffman encoding

--- MPP6/main.go
+++ MPP6/main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"container/heap"
+	"fmt"
+	"math"
 	"mpp6/utils"
 )
 
 func countLetters(sentence string) map[rune]int {
 	letterCount := make(map[rune]int)
 
-	// Use range to iterate over each rune in the string
 	for _, char := range sentence {
 		letterCount[char]++
 	}
@@ -16,18 +17,60 @@ func countLetters(sentence string) map[rune]int {
 	return letterCount
 }
 
-func main() {
-	letterCounts := countLetters("ala ma kota")
+func createInitialPriorityQueue(sentence string) utils.PriorityQueue {
+	letterCounts := countLetters(sentence)
 
 	pq := make(utils.PriorityQueue, len(letterCounts))
 
 	i := 0
 	for key, value := range letterCounts {
 		pq[i] = utils.Item{
-			Name:   key,
+			Name:   string(key),
 			Weight: value,
 		}
 		i++
 	}
 	heap.Init(&pq)
+
+	return pq
+}
+
+func main() {
+	str := "hakuma tata ma"
+	pq := createInitialPriorityQueue(str)
+
+	bitSizeOfChar := 1
+
+	for math.Pow(2, float64(bitSizeOfChar)) < float64(pq.Len()) {
+		bitSizeOfChar++
+	}
+
+	result := fmt.Sprintf("Total bit length of not encoded string is: %d", bitSizeOfChar*len(str))
+
+	fmt.Println(result)
+
+	encodedChars := make(map[string]string)
+
+	for pq.Len() > 1 {
+		left := heap.Pop(&pq).(utils.Item)
+		right := heap.Pop(&pq).(utils.Item)
+
+		newName := left.Name + right.Name
+		newWeight := left.Weight + right.Weight
+
+		newItem := utils.Item{
+			Name:   newName,
+			Weight: newWeight,
+		}
+
+		heap.Push(&pq, newItem)
+
+		for _, char := range left.Name {
+			encodedChars[string(char)] = "0" + encodedChars[string(char)]
+		}
+
+		for _, char := range right.Name {
+			encodedChars[string(char)] = "1" + encodedChars[string(char)]
+		}
+	}
 }
